080ee12c701941da7bf92b73ddd48dccd5308e58
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-18 23:10:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(number)
+  if number <= 1 then return false end
+    is_prime = true
+  for i in 2..number-1
+    if number % i == 0
+      is_prime = false
+    end
+  end
+  if is_prime
+    return TRUE
+  else 
+    return false 
+  end
+end 
