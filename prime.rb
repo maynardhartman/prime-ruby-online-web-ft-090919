95737784c15679b821983b18346d2940774fc2ba
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-18 22:12:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,3 @@
 # Add  code here!
 def prime?(number)
-  number.each { |num|
-   if num == -1
-     retrun FALSE
-   end
-   if num % num = 0 
-     return TRUE
-  else 
-     return FALSE
-  end
-  }
-end
+ensurend
