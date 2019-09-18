c540bc4da2744465ebf66e75457532de9a582976
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-18 22:19:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,15 @@
 # Add  code here!
 def prime?(number)
-  
+  is_prime = true
+  for i in 2..number-1
+    if number % i == 0
+      is_prime = false
+    end
+  end
+  if is_prime
+    puts "#{n} is prime!"
+  else
+    puts "#{n} is not prime."
+  end
+end
 end 
